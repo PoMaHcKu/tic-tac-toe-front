1d7350dd131a17b1678f25c46181d401bb551fef
roman <rom2517pop@gmail.com>
2021-03-20 00:08:38 +0300

    Added web socket connection example

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,15 +1,42 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import * as SockJS from 'sockjs-client'
+import Stomp from 'stompjs'
 
 function Games() {
 
-    // const [games, setGames] = useState([])
+    const [games, setGames] = useState([])
+    const socket = new SockJS("http://localhost:8080/games")
+    const client = Stomp.over(socket);
 
-    // setGames(getGames())
+    useEffect(function () {
+        client.connect({}, function () {
+            client.subscribe('/app/games', function (response) {
+                let body = JSON.parse(response.body);
+                if (body.id) {
+                    setGames([body.id]);
+                }
+            })
+        });
+
+    }, [])
+
+    function getGames() {
+        if (games.length < 1) {
+            return <div>NO ANY GAMES</div>
+        }
+        return games.map(game => <div key={game.id}>{game.id}</div>)
+    }
 
 
     return (
         <div>
-            {/*{games}*/}
+            <div onClick={() => {
+                client.send("/app/games", {}, JSON.stringify({'name': 'hell'}));
+            }}>GET GAMES MANUAL</div>
+            <div>
+
+            </div>
+            {getGames()}
         </div>
     )
 }
